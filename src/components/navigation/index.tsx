1e8c9f5a30184824376f58a218a94253cd3df942
sink <sinkmaster@163.com>
2025-01-26 09:29:01 +0800

    修改拖拽区域

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -6,8 +6,8 @@ import NavLinkItem from './nav-link-item'
 const Navigation: React.FC = () => {
   return (
     <div data-tauri-drag-region className="w-[70px] h-full pt-8 border-r-1 dark:border-neutral-700 text-[12px]">
-      <div className="flex flex-col justify-between h-full">
-        <div className="flex flex-col gap-4 justify-center items-center">
+      <div data-tauri-drag-region className="flex flex-col justify-between h-full">
+        <div className="flex flex-col gap-4 justify-center items-center ">
           <div className="cursor-pointer">
             <NavLinkItem icon={<House className="h-5" />} link="/home" label="home" />
           </div>
